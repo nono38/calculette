40c01f2fe32dd359cd9bd46cc42191b9f15140a4
nono38 <74985866+nono38@users.noreply.github.com>
2020-12-23 12:39:25 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    cacapipicrotte = -1 + cacapipicrotte
+    basic.showNumber(cacapipicrotte)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(3 * cacapipicrotte)
+})
+input.onButtonPressed(Button.B, function () {
+    cacapipicrotte = 1 + cacapipicrotte
+    basic.showNumber(cacapipicrotte)
+})
+let cacapipicrotte = 0
+cacapipicrotte = 0
+basic.forever(function () {
+	
+})
